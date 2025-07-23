6e2626a1306a8f3ee29ab862a105ed368342ca45
batukizmazoglu <batukl89@gmail.com>
2025-07-23 15:33:35 +0300

    Deploy test

--- app/(static)/templates/[category]/page.tsx
+++ app/(static)/templates/[category]/page.tsx
@@ -1,13 +1,13 @@
 import { redirect } from 'next/navigation'
 
 interface CategoryPageProps {
-  params: Promise<{
+  params: {
     category: string
-  }>
+  }
 }
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
-  const { category } = await params;
+  const { category } = params;
   // You could add logic here to handle different categories
   // For now, we'll redirect all category roots to login
   redirect('/templates/authentication/login')
